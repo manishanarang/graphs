3de929058b33c9cf184de14a42753c4321271d1b
manishanarang <160212455+manishanarang@users.noreply.github.com>
2024-03-31 12:25:05 +0530

    Time: 354 ms (36.54%), Space: 61.8 MB (53.49%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -2,55 +2,62 @@ class Solution {
     public int minCostConnectPoints(int[][] points) {
         int n = points.length;
         int cost = 0;
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b)->a[0]-b[0]);
-        HashSet<Integer> visited = new HashSet<>();
-        HashMap<Integer, List<List<Integer>>> adj = new HashMap<>();
-
         
-        
+        List<int[]> edges = new ArrayList<>();
         for(int i = 0; i<n;i++){
-            for(int j = 0;j<n;j++){
-                adj.put(i, new ArrayList<>());
-                adj.put(j, new ArrayList<>());
+            for(int j=i+1;j<n;j++){
+               int wt = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
+                edges.add(new int[]{i,j,wt});
             }
         }
-        for(int i = 0; i <n; i++){
-            int x1 = points[i][0];
-            int y1 = points[i][1];
-
-            for(int j = i+1; j <n; j++){
-                int x2 = points[j][0];
-                int y2 = points[j][1];
-
-                int dist = Math.abs(x1-x2) + Math.abs(y1-y2);
-
-                adj.computeIfAbsent(i, 
-                k -> new ArrayList<>()).add(List.of(dist, j));
-                adj.computeIfAbsent(j, 
-                k -> new ArrayList<>()).add(List.of(dist, i));
-            }
-        }
-
-        minHeap.add(new int[]{0,0});
         
-        while(visited.size()!=n){
-            int[] costAndVertex = minHeap.poll();
-            int vertex = costAndVertex[1];
+        Collections.sort(edges, (a,b)->a[2]-b[2]);
+        int processed = 0;
+        UnionFind uf = new UnionFind(n);
+        for(int[] edge: edges){
+            if(processed < n && uf.union(edge[0], edge[1])){
+                cost+=edge[2];
+                processed++;
+            }
+        }
+        return cost;
+    }
 
-            if(!visited.contains(vertex)){
-                cost+=costAndVertex[0];
-                visited.add(vertex);
+    class UnionFind{
+        int[] parent;
+        int[] rank;
 
-                for(List<Integer> neighbour: adj.get(vertex)){
-                    if(!visited.contains(neighbour.get(1))){
-                        minHeap.add(new int[]{
-                        neighbour.get(0),
-                        neighbour.get(1)});
-                    }
-                }
+        public UnionFind(int n){
+            parent = new int[n+1];
+            rank = new int[n+1];
+            for(int i = 0; i <=n; i++){
+                this.parent[i] = i;
+                this.rank[i] = 1;
             }
         }
 
-        return cost;
+        public int find(int x){
+            if(x!=parent[x]){
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+
+        public boolean union(int x, int y){
+            int p1 = find(x);
+            int p2 = find(y);
+
+            if(p1 == p2) return false;
+
+            if(rank[p1] > rank[p2]){
+                parent[p2] = p1;
+                rank[p1] = rank[p2] + 1;
+            }
+            else{
+                parent[p1] = p2;
+                rank[p2] = rank[p1] + 1;
+            }
+            return true;
+        }
     }
 }
