f58fd6824818ed629eb1f74edf521eeb933f7d63
manishanarang <160212455+manishanarang@users.noreply.github.com>
2024-03-31 19:45:41 +0530

    Time: 21 ms (41.87%), Space: 48 MB (64.41%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int networkDelayTime(int[][] times, int n, int k) {
+        Map<Integer, List<int[]>> adj = new HashMap<>();
+        
+        for(int[] time: times){
+            adj.computeIfAbsent(time[0], key-> new ArrayList<>()).add(new int[]{time[1], time[2]});
+        }
+        int time = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[1]-b[1]);
+        pq.add(new int[]{k, 0});
+
+        HashSet<Integer> visited= new HashSet<>();
+        
+        while(!pq.isEmpty()){
+            int[] edge = pq.poll();
+            
+            int u = edge[0];
+            int cost = edge[1];
+         
+            if(visited.contains(u)) continue;
+            
+            visited.add(u);
+            time = Math.max(time, cost);
+            
+            if(!adj.containsKey(u)){
+                continue;
+            }
+            
+            for(int[] neighbour : adj.get(u)){
+                
+                int v = neighbour[0];
+                if(visited.contains(v)) continue;
+                
+                int wt = neighbour[1];
+                pq.offer(new int[]{v, cost + wt});
+            }
+        }
+        return visited.size() == n ? time : -1;
+    }
+}
